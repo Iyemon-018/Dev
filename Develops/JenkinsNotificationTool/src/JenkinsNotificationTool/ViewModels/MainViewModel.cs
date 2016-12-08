5b92c074078634a225db7268536b8711728fc27e
イシイケンタ <iyemon018@gmail.com>
2016-12-08 21:14:31 +0900

    無視リストに加えられていたのでプロジェクトに追加した。

---
+++ Develops/JenkinsNotificationTool/src/JenkinsNotificationTool/ViewModels/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿namespace JenkinsNotificationTool.ViewModels
+{
+    using System.Windows;
+    using JenkinsNotification.Core.ComponentModels;
+    using JenkinsNotification.Core.Services;
+    using JenkinsNotification.CustomControls.Services;
+    using Microsoft.Practices.Prism.Commands;
+
+    public class MainViewModel : ApplicationViewModelBase
+    {
+        private readonly IBalloonTipService _balloonTipService;
+
+        public MainViewModel()
+            : this(new DialogService(), null)
+        {
+            
+        }
+
+        public MainViewModel(IDialogService dialogService, IBalloonTipService balloonTipService) : base(dialogService)
+        {
+            _balloonTipService = balloonTipService;
+
+            //
+            // 各コマンドの初期化を行う。
+            //
+
+            //
+            // 終了コマンド
+            // コンテキストメニューからメッセージボックスを表示すると即時クローズされてしまうので
+            // メッセージ無しで終了する。
+            //
+            ExitCommand = new DelegateCommand(() =>ApplicationManager.Shutdown());
+
+            ConfigurationCommand = new DelegateCommand(() =>
+                                                       {
+                                                           // TODO 構成情報画面を表示する。
+                                                       });
+
+
+            ReceivedNotificationListCommand = new DelegateCommand(() =>
+                                                                  {
+                                                                      // TODO 通知受信履歴一覧を表示する。
+                                                                  });
+        }
+
+        public DelegateCommand ExitCommand { get; private set; }
+
+        public DelegateCommand ConfigurationCommand { get; private set; }
+
+        public DelegateCommand ReceivedNotificationListCommand { get; private set; }
+    }
+}
---
+++ Develops/JenkinsNotificationTool/src/JenkinsNotificationTool/Views/MainView.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace JenkinsNotificationTool.Views
+{
+    using System.ComponentModel;
+    using JenkinsNotification.Core.Services;
+    using JenkinsNotification.CustomControls.Services;
+    using JenkinsNotificationTool.ViewModels;
+
+    /// <summary>
+    /// MainView.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+
+            DataContext = new MainViewModel(new DialogService(), new BalloonTipService(TaskbarIcon));
+        }
+
+        private void MainView_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            TaskbarIcon.Dispose();
+            base.OnClosing(e);
+        }
+    }
+}
