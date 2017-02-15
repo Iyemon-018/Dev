ad170927a64ff079865437ce0d9a887f9bb5d0b3
(MC) Ishii <kishii@microcircus.com>
2017-02-15 18:28:52 +0900

    実装

--- Samples/CarouselPanelSample01/CarouselPanelSample01/MainWindow.xaml.cs
+++ Samples/CarouselPanelSample01/CarouselPanelSample01/MainWindow.xaml.cs
@@ -1,20 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace CarouselPanelSample01
+﻿namespace CarouselPanelSample01
 {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Windows;
+
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
@@ -23,6 +15,34 @@ namespace CarouselPanelSample01
         public MainWindow()
         {
             InitializeComponent();
+        }
+        
+        private void NextButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            // ひとつ前の要素を選択する。
+            var selectedIndex = pathListBox.SelectedIndex;
+            if (selectedIndex < pathListBox.Items.Count - 1)
+            {
+                pathListBox.SelectedIndex++;
+            }
+            else
+            {
+                pathListBox.SelectedIndex = 0;
+            }
+        }
+
+        private void PreviousButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            // 次の要素を選択する。
+            var selectedIndex = pathListBox.SelectedIndex;
+            if (0 < selectedIndex)
+            {
+                pathListBox.SelectedIndex--;
+            }
+            else
+            {
+                pathListBox.SelectedIndex = pathListBox.Items.Count - 1;
+            }
         }
     }
 }
