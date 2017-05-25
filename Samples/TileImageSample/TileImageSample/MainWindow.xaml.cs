79fa82b1914dd572f26edb41fef559f5826a222b
イシイケンタ <iyemon018@gmail.com>
2017-05-25 23:14:01 +0900

    タッチ フィードバックの無効化サンプルを追加した。

--- Samples/TileImageSample/TileImageSample/MainWindow.xaml.cs
+++ Samples/TileImageSample/TileImageSample/MainWindow.xaml.cs
@@ -1,28 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace TileImageSample
+﻿namespace TileImageSample
 {
+    using System.Windows;
+    using System.Windows.Input;
+
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Fields
+
+        private Point _point;
+
+        #endregion
+
+        #region Ctor
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        #endregion
+
+        #region Methods
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Rect viewport = ImageTileBrush.Viewport;
+            viewport.Offset(10000000, 10000000);
+            ImageTileBrush.Viewport = viewport;
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            _point = e.GetPosition(this);
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                Point point = e.GetPosition(this);
+                Vector moveVector = new Vector(point.X - _point.X, point.Y - _point.Y);
+                Rect viewport = ImageTileBrush.Viewport;
+                viewport.Offset(moveVector);
+                ImageTileBrush.Viewport = viewport;
+                _point = point;
+            }
+        }
+
+        #endregion
     }
 }
---
+++ Samples/TouchFeedbackDisable/TouchFeedbackDisable/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿namespace TouchFeedbackDisable
+{
+    using System.Runtime.InteropServices;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Shapes;
+
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Fields
+
+        private TouchPoint _point;
+
+        #endregion
+
+        #region Ctor
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void DrawLinePanel_OnPreviewTouchDown(object sender, TouchEventArgs e)
+        {
+            Canvas drawLinePanel = sender as Canvas;
+            if (drawLinePanel != null)
+            {
+                _point = e.GetTouchPoint(drawLinePanel);
+            }
+        }
+
+        private void DrawLinePanel_OnPreviewTouchMove(object sender, TouchEventArgs e)
+        {
+            Canvas drawLinePanel = sender as Canvas;
+            if (drawLinePanel != null)
+            {
+                TouchPoint currentPoint = e.GetTouchPoint(drawLinePanel);
+                Line line = new Line();
+                line.Stroke = Brushes.White;
+                line.StrokeThickness = 1;
+                line.X1 = _point.Position.X;
+                line.Y1 = _point.Position.Y;
+                line.X2 = currentPoint.Position.X;
+                line.Y2 = currentPoint.Position.Y;
+                drawLinePanel.Children.Add(line);
+                _point = currentPoint;
+            }
+        }
+
+        private void TouchFeedback_OnChecked(object sender, RoutedEventArgs e)
+        {
+            TouchFeedbackStatus(true);
+        }
+
+        private void TouchFeedback_OnUnchecked(object sender, RoutedEventArgs e)
+        {
+            TouchFeedbackStatus(false);
+        }
+
+        private void TouchFeedbackStatus(bool enable)
+        {
+            Stylus.SetIsTouchFeedbackEnabled(this, enable);
+            Stylus.SetIsTapFeedbackEnabled(this, enable);
+            Stylus.SetIsPressAndHoldEnabled(this, enable);
+            Cursor = enable ? Cursors.Arrow : Cursors.None;
+        }
+
+        #endregion
+    }
+}
