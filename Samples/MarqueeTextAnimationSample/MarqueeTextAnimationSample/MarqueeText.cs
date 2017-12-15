036f18da1d87c13f520a6bc94afaefbf8c16b864
AYATAKA\Kishii <kishii@microcircus.com>
2017-12-15 18:05:00 +0900

    MarqueeTextのサンプルプロジェクトを新規追加

---
+++ Samples/MarqueeTextAnimationSample/MarqueeTextAnimationSample/MarqueeText.cs
@@ -0,0 +1,12 @@
+﻿namespace MarqueeTextAnimationSample
+{
+    using System.Windows.Controls;
+
+    public class MarqueeText : Control
+    {
+        static MarqueeText()
+        {
+            
+        }
+    }
+}
