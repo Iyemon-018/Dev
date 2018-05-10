0324f11648db0a8c539b85249d05fa296a8b69c3
iyemon018 <iyemon018@gmail.com>
2018-05-10 21:15:46 +0900

    新規追加

---
+++ Samples/GCP.VisionAPI.Sample/GCP.VisionAPI.Sample/Bootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using GCP.VisionAPI.Sample.Views;
+using System.Windows;
+using Prism.Modularity;
+using Microsoft.Practices.Unity;
+using Prism.Unity;
+
+namespace GCP.VisionAPI.Sample
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            var moduleCatalog = (ModuleCatalog)ModuleCatalog;
+            //moduleCatalog.AddModule(typeof(YOUR_MODULE));
+        }
+    }
+}
