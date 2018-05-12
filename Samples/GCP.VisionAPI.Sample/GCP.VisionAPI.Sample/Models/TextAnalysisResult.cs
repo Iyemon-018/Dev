8bd6af7048cb078584981a15e09fec8f52367574
iyemon018 <iyemon018@gmail.com>
2018-05-13 00:47:16 +0900

    変換する機能まで

---
+++ Samples/GCP.VisionAPI.Sample/GCP.VisionAPI.Sample/Models/TextAnalysisResult.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Google.Cloud.Vision.V1;
+using Prism.Mvvm;
+
+namespace GCP.VisionAPI.Sample.Models
+{
+    public class TextAnalysisResult : BindableBase
+    {
+        public static TextAnalysisResult Build(TextAnnotation analysisResult)
+        {
+            Page page = analysisResult.Pages.FirstOrDefault();
+            TextAnalysisResult result = new TextAnalysisResult();
+
+            result.DetectedLanguages.AddRange(page.Property.DetectedLanguages.Select(x => x.LanguageCode).ToArray());
+            result.Width = page.Width;
+            result.Height = page.Height;
+
+            return result;
+        }
+
+        public List<string> DetectedLanguages { get; } = new List<string>();
+
+        private int _width;
+
+        public int Width
+        {
+            get => _width;
+            private set => SetProperty(ref _width, value);
+        }
+
+        private int _height;
+
+        public int Height
+        {
+            get => _height;
+            private set => SetProperty(ref _height, value);
+        }
+
+    }
+}
--- Samples/GCP.VisionAPI.Sample/GCP.VisionAPI.Sample/ViewModels/MainWindowViewModel.cs
+++ Samples/GCP.VisionAPI.Sample/GCP.VisionAPI.Sample/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,9 @@
-﻿using GCP.VisionAPI.Sample.Services;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Media.Imaging;
+using GCP.VisionAPI.Sample.Services;
+using Google.Cloud.Vision.V1;
 using Prism.Commands;
 using Prism.Mvvm;
 
@@ -22,6 +27,25 @@ namespace GCP.VisionAPI.Sample.ViewModels
             set => SetProperty(ref _fileName, value);
         }
 
+        private BitmapImage _imageSource;
+
+        public BitmapImage ImageSource
+        {
+            get => _imageSource;
+            private set => SetProperty(ref _imageSource, value);
+        }
+
+
+        public ObservableCollection<string> AnalysisResults { get; } = new ObservableCollection<string>();
+
+        private string _analysisResultFlatten;
+
+        public string AnalysisResultFlatten
+        {
+            get => _analysisResultFlatten;
+            private set => SetProperty(ref _analysisResultFlatten, value);
+        }
+
 
         public DelegateCommand OpenSelectionDialogCommand { get; private set; }
 
@@ -32,14 +56,35 @@ namespace GCP.VisionAPI.Sample.ViewModels
         public MainWindowViewModel(ISelectionDialogService selectionDialogService)
         {
             this.selectionDialogService = selectionDialogService;
-            OpenSelectionDialogCommand = new DelegateCommand(() => FileName = this.selectionDialogService.SelectFile());
+            OpenSelectionDialogCommand = new DelegateCommand(() =>
+            {
+                FileName = this.selectionDialogService.SelectFile();
+                ImageSource = new BitmapImage(new Uri(FileName));
+            });
 
             AnalyzeCommand = new DelegateCommand(ExecuteAnalyzeCommand);
         }
 
         private void ExecuteAnalyzeCommand()
         {
-            
+            NotifyMessage = "画像解析（OCR）を開始する。";
+            TextAnnotation response = getAnalysisResult(FileName);
+            NotifyMessage = "解析結果を集計する。";
+            Block[] blocks = response.Pages.SelectMany(x => x.Blocks).ToArray();
+            Paragraph[] paragraphs = blocks.SelectMany(x => x.Paragraphs).ToArray();
+            Word[] words = paragraphs.SelectMany(x => x.Words).ToArray();
+            AnalysisResults.Clear();
+            AnalysisResults.AddRange(words.Select(x => x.ToString()));
+            AnalysisResultFlatten = string.Join(Environment.NewLine, AnalysisResults);
+            NotifyMessage = "解析が完了した。";
+        }
+
+        private TextAnnotation getAnalysisResult(string fileName)
+        {
+            Image image = Image.FromFile(fileName);
+            ImageAnnotatorClient client = ImageAnnotatorClient.Create();
+            TextAnnotation response = client.DetectDocumentText(image);
+            return response;
         }
     }
 }
