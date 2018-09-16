85f55fa0df6e9bc16f60775242ae9c8ad4e7f91e
iyemon018 <iyemon018@gmail.com>
2018-09-16 17:49:13 +0900

    ProgressDialog の外観を設定

--- Samples/MaterialDesign.Dialog.Example/MaterialDesign.Dialog.Example/Dialogs/ViewModels/ProgressDialogViewModel.cs
+++ Samples/MaterialDesign.Dialog.Example/MaterialDesign.Dialog.Example/Dialogs/ViewModels/ProgressDialogViewModel.cs
@@ -1,7 +1,32 @@
-﻿namespace MaterialDesign.Dialog.Example.Dialogs.ViewModels
+﻿using Prism.Mvvm;
+
+namespace MaterialDesign.Dialog.Example.Dialogs.ViewModels
 {
-    public class ProgressDialogViewModel
+    public class ProgressDialogViewModel : BindableBase
     {
-        
+        private string _message;
+
+        public string Message
+        {
+            get => _message;
+            set => SetProperty(ref _message, value);
+        }
+
+        private int _count;
+
+        public int Count
+        {
+            get => _count;
+            set => SetProperty(ref _count, value);
+        }
+
+        private int _progress;
+
+        public int Progress
+        {
+            get => _progress;
+            set => SetProperty(ref _progress, value);
+        }
+
     }
 }
