c2bb2c3ffec5a5dab949f6ae879156e3026306ad
iyemon018 <iyemon018@gmail.com>
2018-09-16 16:23:21 +0900

    Shell の最低限の外観を作成

--- Samples/MaterialDesign.Dialog.Example/MaterialDesign.Dialog.Example/ViewModels/ShellViewModel.cs
+++ Samples/MaterialDesign.Dialog.Example/MaterialDesign.Dialog.Example/ViewModels/ShellViewModel.cs
@@ -1,7 +1,15 @@
-﻿namespace MaterialDesign.Dialog.Example.ViewModels
+﻿using System.Windows.Input;
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace MaterialDesign.Dialog.Example.ViewModels
 {
-    public class ShellViewModel
+    public class ShellViewModel : BindableBase
     {
-        
+        public ShellViewModel()
+        {
+            ShutdownCommand = new DelegateCommand(() => App.Current.MainWindow.Close());
+        }
+        public ICommand ShutdownCommand { get; private set; }
     }
 }
