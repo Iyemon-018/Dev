512f68fd4d8721ee3183d5e7ddb292014adb1d68
iyemon018 <iyemon018@gmail.com>
2018-12-22 19:56:56 +0900

    地図の操作機能を追加

--- Samples/XamlIslands.Wpf/XamlIslands.Wpf/MainWindow.xaml.cs
+++ Samples/XamlIslands.Wpf/XamlIslands.Wpf/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿namespace XamlIslands.Wpf
 {
+    using System;
+    using System.Threading.Tasks;
     using System.Windows;
+    using System.Windows.Controls;
     using Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT;
 
     /// <summary>
@@ -8,6 +11,8 @@
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool _isLoadedMap;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -15,9 +20,45 @@
 
         private async void MapControl_OnLoaded(object sender, RoutedEventArgs e)
         {
-            BasicGeoposition cityPosition = new BasicGeoposition { Latitude = 34.702476, Longitude = 135.495946};
+            BasicGeoposition cityPosition = new BasicGeoposition {Latitude = 34.702476, Longitude = 135.495946};
             Geopoint         cityCenter   = new Geopoint(cityPosition);
-            bool result = await MapControl.TrySetViewAsync(center:cityCenter, zoomLevel:14);
+
+            while (!_isLoadedMap)
+            {
+                // animation が設定されていると座標移動に時間がかかる。
+                // 初期座標を設定するならMapAnimationKind.None がよさげ。
+                bool result = await MapControl.TrySetViewAsync(center: cityCenter, zoomLevel: 14, heading: 0
+                                                             , desiredPitch: 0, animation: MapAnimationKind.None);
+                if (!result)
+                {
+                    // １回では初期座標の設定が失敗する可能性が高い。
+                    // 成功するまで間隔おいて再チャレンジする。
+                    await Task.Delay(TimeSpan.FromMilliseconds(500));
+                    continue;
+                }
+
+                _isLoadedMap = true;
+            }
+
+            ZoomSlider.Value = 14;
+        }
+
+        private async void ZoomSlider_OnValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            await MapControl.TryZoomToAsync((double) (sender as Slider)?.Value);
+        }
+
+        private async void RotateSlider_OnValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (!_isLoadedMap)
+            {
+                return;
+            }
+
+            // 現在の座標はActualCamera.Location で取得できる。
+            // 拡大率の取得方法は今の所わかっていない。
+            bool result = await MapControl.TrySetViewAsync(center: MapControl.ActualCamera.Location, zoomLevel: 14
+                                                         , heading: RotateSlider.Value, desiredPitch: 0);
         }
     }
 }
