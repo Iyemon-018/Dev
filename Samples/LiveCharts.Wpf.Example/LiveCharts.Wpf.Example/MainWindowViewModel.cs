10b56137a6021823eb36a7e460d293dbf7a1e5f6
iyemon018@gmail.com <iyemon018>
2019-03-03 16:49:56 +0900

    新規追加

---
+++ Samples/LiveCharts.Wpf.Example/LiveCharts.Wpf.Example/MainWindowViewModel.cs
@@ -0,0 +1,66 @@
+﻿namespace LiveCharts.Wpf.Example
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Prism.Mvvm;
+
+    public class MainWindowViewModel : BindableBase
+    {
+        public MainWindowViewModel()
+        {
+            List<WorkTime> workTimes = new List<WorkTime>
+                            {
+                                new WorkTime
+                                {
+                                    Date       = DateTime.Today.AddDays(-3)
+                                  , ActualTime = 900
+                                }
+                              , new WorkTime
+                                {
+                                    Date       = DateTime.Today.AddDays(-3)
+                                  , ActualTime = 7200
+                                }
+                              , new WorkTime
+                                {
+                                    Date       = DateTime.Today.AddDays(-2)
+                                  , ActualTime = 1800
+                                }
+                              , new WorkTime
+                                {
+                                    Date       = DateTime.Today.AddDays(-2)
+                                  , ActualTime = 6000
+                                }
+                              , new WorkTime
+                                {
+                                    Date       = DateTime.Today.AddDays(-2)
+                                  , ActualTime = 7200
+                                }
+                            };
+
+            Series = new SeriesCollection();
+            Series.AddRange(workTimes.GroupBy(x => x.Date)
+                                     .Select(x => new PieSeries
+                                                  {
+                                                      Title = x.Key.ToString("yyyy/MM/dd")
+                                                    , Values = new ChartValues<int>(new[]
+                                                                                    {
+                                                                                        x.Sum(v => v.ActualTime)
+                                                                                    })
+                                                    , DataLabels = true
+                                                    , LabelPoint = p => $"{p.SeriesView.Title}{Environment.NewLine} {p.Participation:P}"
+                                                    , FontSize = 18.0d
+                                                  }));
+
+            SeriesColors = new ColorsCollection();
+            SeriesColors.AddRange(new[] {"#FFE65100", "#FFEF6C00", "#FFF57C00", "#FFFB8C00", "#FFFF9800"}
+                                  .Select(System.Windows.Media.ColorConverter.ConvertFromString)
+                                  .OfType<System.Windows.Media.Color>()
+                                  .ToList());
+        }
+
+        public SeriesCollection Series { get; }
+
+        public ColorsCollection SeriesColors { get; }
+    }
+}
---
+++ Samples/LiveCharts.Wpf.Example/LiveCharts.Wpf.Example/WorkTime.cs
@@ -0,0 +1,48 @@
+﻿namespace LiveCharts.Wpf.Example
+{
+    using System;
+    using Prism.Mvvm;
+
+    /// <summary>
+    /// ある作業単位の実施時間を保持するクラスです。
+    /// この作業実施時間は１日毎に異なる情報として扱われます。
+    /// </summary>
+    /// <seealso cref="Prism.Mvvm.BindableBase" />
+    public class WorkTime : BindableBase
+    {
+        private DateTime _date;
+
+        /// <summary>
+        /// 作業を実施した日付を設定、または取得します。
+        /// </summary>
+        public DateTime Date
+        {
+            get => _date;
+            set => SetProperty(ref _date, value);
+        }
+
+        private int _actualTime;
+
+        /// <summary>
+        /// 実作業時間を設定、または取得します。
+        /// 単位は"秒"です。
+        /// </summary>
+        public int ActualTime
+        {
+            get => _actualTime;
+            set => SetProperty(ref _actualTime, value);
+        }
+
+        private string _comment;
+
+        /// <summary>
+        /// 実施した作業内容を設定、または取得します。
+        /// </summary>
+        public string Comment
+        {
+            get => _comment;
+            set => SetProperty(ref _comment, value);
+        }
+
+    }
+}
