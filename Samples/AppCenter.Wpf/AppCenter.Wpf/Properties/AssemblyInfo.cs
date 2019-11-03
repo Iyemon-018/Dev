e148b6659260966141fdfa7a16e987c02b070401
iyemon018@gmail.com <iyemon018>
2019-11-03 19:25:17 +0900

    アセンブリ情報を追加した。

--- Samples/AppCenter.Wpf/AppCenter.Wpf/Properties/AssemblyInfo.cs
+++ Samples/AppCenter.Wpf/AppCenter.Wpf/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 // ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
 // 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
 // その型の ComVisible 属性を true に設定してください。
-[assembly: ComVisible(false)]
+//[assembly: ComVisible(false)]
 
 //ローカライズ可能なアプリケーションのビルドを開始するには、
 //.csproj ファイルの <UICulture>CultureYouAreCodingWith</UICulture> を
@@ -31,14 +31,14 @@ using System.Windows;
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
 
-[assembly: ThemeInfo(
-    ResourceDictionaryLocation.None, //テーマ固有のリソース ディクショナリが置かれている場所
-                                     //(リソースがページ、
-                                     // またはアプリケーション リソース ディクショナリに見つからない場合に使用されます)
-    ResourceDictionaryLocation.SourceAssembly //汎用リソース ディクショナリが置かれている場所
-                                              //(リソースがページ、
-                                              //アプリケーション、またはいずれのテーマ固有のリソース ディクショナリにも見つからない場合に使用されます)
-)]
+//[assembly: ThemeInfo(
+//    ResourceDictionaryLocation.None, //テーマ固有のリソース ディクショナリが置かれている場所
+//                                     //(リソースがページ、
+//                                     // またはアプリケーション リソース ディクショナリに見つからない場合に使用されます)
+//    ResourceDictionaryLocation.SourceAssembly //汎用リソース ディクショナリが置かれている場所
+//                                              //(リソースがページ、
+//                                              //アプリケーション、またはいずれのテーマ固有のリソース ディクショナリにも見つからない場合に使用されます)
+//)]
 
 
 // アセンブリのバージョン情報は次の 4 つの値で構成されています:
@@ -51,5 +51,5 @@ using System.Windows;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-//[assembly: AssemblyVersion("1.0.0.0")]
-//[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
